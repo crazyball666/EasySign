0776f3a3453ed04ee3c318ebf8d5f0c33d04ed7b
crazyball <crazyball6666@gamil.com>
2026-05-31 12:39:55 +0800

    fix: register ipa quick look type

--- Tests/IPAQuickLookExtensionSourceTests.sh
+++ Tests/IPAQuickLookExtensionSourceTests.sh
@@ -2,10 +2,12 @@
 set -eu
 
 PROJECT="EasySign.xcodeproj/project.pbxproj"
+APP_INFO="EasySign/Info.plist"
 EXT_INFO="EasySignQuickLook/Info.plist"
 EXT_PROVIDER="EasySignQuickLook/PreviewProvider.swift"
 HTML_RENDERER="EasySign/ResignService/Model/IPAPreviewHTMLRenderer.swift"
 
+test -f "$APP_INFO"
 test -f "$EXT_INFO"
 test -f "$EXT_PROVIDER"
 test -f "$HTML_RENDERER"
@@ -14,12 +16,25 @@ rg -n "EasySignQuickLook" "$PROJECT" >/dev/null
 rg -n "EasySignQuickLook\\.appex in Embed Foundation Extensions" "$PROJECT" >/dev/null
 rg -n "com\\.apple\\.product-type\\.app-extension" "$PROJECT" >/dev/null
 rg -n "PBXTargetDependency" "$PROJECT" >/dev/null
+rg -n "INFOPLIST_FILE = EasySign/Info\\.plist" "$PROJECT" >/dev/null
+
+rg -n "CFBundleDocumentTypes" "$APP_INFO" >/dev/null
+rg -n "LSItemContentTypes" "$APP_INFO" >/dev/null
+rg -n "UTImportedTypeDeclarations" "$APP_INFO" >/dev/null
+rg -n "UTTypeIdentifier" "$APP_INFO" >/dev/null
+rg -n "com\\.apple\\.itunes\\.ipa" "$APP_INFO" >/dev/null
+rg -n "public\\.filename-extension" "$APP_INFO" >/dev/null
+rg -n "<string>ipa</string>" "$APP_INFO" >/dev/null
 
 rg -n "com\\.apple\\.quicklook\\.preview" "$EXT_INFO" >/dev/null
 rg -n "QLIsDataBasedPreview" "$EXT_INFO" >/dev/null
+rg -n "UTImportedTypeDeclarations" "$EXT_INFO" >/dev/null
+rg -n "UTTypeIdentifier" "$EXT_INFO" >/dev/null
 rg -n "com\\.apple\\.itunes\\.ipa" "$EXT_INFO" >/dev/null
+rg -n "public\\.filename-extension" "$EXT_INFO" >/dev/null
+rg -n "<string>ipa</string>" "$EXT_INFO" >/dev/null
+rg -n "UTTypeConformsTo" "$EXT_INFO" >/dev/null
 rg -n "public\\.zip-archive" "$EXT_INFO" >/dev/null
-rg -n "com\\.apple\\.application-bundle" "$EXT_INFO" >/dev/null
 
 rg -n "class PreviewProvider: QLPreviewProvider, QLPreviewingController" "$EXT_PROVIDER" >/dev/null
 rg -n "providePreview\\(for request: QLFilePreviewRequest\\)" "$EXT_PROVIDER" >/dev/null
